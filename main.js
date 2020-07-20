ce051664ee8c52be65df60fc31066f0857511919
Jatuphum Tungsilsukchai <43282496+jatu-studiobox@users.noreply.github.com>
2020-07-21 04:32:28 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -18,12 +18,13 @@
   async function GatheringResult() {
       try {
           //const result = await fetch(`https://us-central1-mineko-1.cloudfunctions.net/SearchAndGatheringPsc3YrsLottoPrizeListByDate?sdate=${sdate}`,{mode: 'cors'});
-          const result = await fetch(`https://us-central1-test-exe91.cloudfunctions.net/test`);
+          const response = await fetch(`https://us-central1-test-exe91.cloudfunctions.net/test`);
           // const url = `https://us-central1-test-exe91.cloudfunctions.net/test`; // site that doesn’t send Access-Control-*
           // fetch(url)
           //     .then(response => response.text())
           //     .then(contents => console.log(contents))
           //     .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+          const result = await response.json();
           console.log(result);
       } catch (error) {
           console.log(error);
